105052f2819b1a88b685f82560689a1878a4b83e
Palencia503 <155649768+Palencia503@users.noreply.github.com>
2026-03-28 20:37:27 +0100

    Update BackupDirectorio.sh

--- bash/Semana-3-BackupDeUnDirectorio/BackupDirectorio.sh
+++ bash/Semana-3-BackupDeUnDirectorio/BackupDirectorio.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-# Semana 3 -  Copia de seguridad directa
-ORIGEN="/home/jp/Documents"    # Carpeta a salvar
-DESTINO="/home/jp/Backups"     # Carpeta de destino
-FECHA=$(date +%Y-%m-%d_%H%M)   # Marca de tiempo
+#Semana 3 -  Copia de seguridad directa
+ORIGEN="/home/jp/Documents"    #Carpeta a salvar
+DESTINO="/home/jp/Backups"     #Carpeta de destino
+FECHA=$(date +%Y-%m-%d_%H%M)   #Marca de tiempo
 
-# 1. Crear el directorio de destino con la fecha para no sobrescribir la anterior
+#1.Crear el directorio de destino con la fecha para no sobrescribir la anterior
 mkdir -p "$DESTINO/copia_$FECHA"
 
-# 2. Copiar todo el contenido de forma recursiva
+#2.Copiar todo el contenido de forma recursiva
 cp -r "$ORIGEN/." "$DESTINO/copia_$FECHA/"
 
-# 3. Verificación simple
+#3.Verificación simple
 if [ $? -eq 0 ]; then
     echo "[$FECHA] Backup copiado correctamente en $DESTINO/copia_$FECHA"
 else
