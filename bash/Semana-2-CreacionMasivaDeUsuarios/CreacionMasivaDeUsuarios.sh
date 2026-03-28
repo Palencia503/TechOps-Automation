b563eaf5120b9a0dfd3e9923db299f2752eed245
Palencia503 <155649768+Palencia503@users.noreply.github.com>
2026-03-28 19:14:45 +0100

    Create CreacionMasivaDeUsuarios.sh

---
+++ bash/Semana-2-CreacionMasivaDeUsuarios/CreacionMasivaDeUsuarios.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Semana 2 - Creación masiva de usuarios
+input="usuarios.csv"
+
+while IFS= read -r user
+do
+  sudo useradd -m -s /bin/bash "$user"
+  echo "Usuario $user creado con éxito."
+done < "$input"
